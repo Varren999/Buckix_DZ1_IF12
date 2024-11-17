ea80d149e2caba6db898daf5db83ae0f89c435e9
Varren999 <varren999@gmail.com>
2024-11-17 13:56:15 +0200

    Work version

---
+++ Buckix_DZ1_IF12/Program.cs
@@ -0,0 +1,21 @@
+﻿//===================================================
+// Задание: Даны три числа. Найти наименьшее из них.
+//===================================================
+
+using System;
+
+namespace Buckix_DZ1_IF12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(100), b = rnd.Next(100), c = rnd.Next(100);
+            Console.WriteLine($"Даны три числа.\na = {a}\nb = {b}\nc = {c}");
+            Console.Write("Наименьшее число из них ");
+            Console.WriteLine(a < b ? a < c ? a : c : b < c ? b : c);
+            Console.ReadKey();
+        }
+    }
+}
